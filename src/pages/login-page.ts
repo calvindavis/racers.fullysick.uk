24bef93156dac8bcd8f8f0b6e5039eb6ff0d02bf
Calvin Davis <hi@calvindavis.co.uk>
2026-07-16 23:59:02 +0100

    chore: test Cloudflare deployment

--- src/pages/login-page.ts
+++ src/pages/login-page.ts
@@ -4,7 +4,8 @@ import { customElement } from "lit/decorators.js";
 @customElement("login-page")
 export class LoginPage extends LitElement {
   render() {
-    return html`<h1>Login</h1>`;
+    return html`<h1>Login</h1>
+      <p>Coming soon!</p>`;
   }
 }
 
